ffdb06bd421b27955ef879ae693e92eb21eb7ef0
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-06-13 13:36:17 +0545

    Home and about pages

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// For Navbar
+
+let button = document.querySelector(".hamburger");
+let navlist = document.querySelector(".navlink-outwrapper");
+console.log(navlist);
+
+button.addEventListener("click", () => {
+  navlist.classList.toggle("show");
+});
+
+
+
+var card = document.getElementById('card')
