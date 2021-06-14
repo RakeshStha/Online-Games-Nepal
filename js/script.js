8ee993fb6f00bf3c2e221dfa9825fd88e1c036e9
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-06-14 18:12:28 +0545

    Singleplayer game + downloads

--- js/script.js
+++ js/script.js
@@ -11,3 +11,49 @@ button.addEventListener("click", () => {
 
 
 var card = document.getElementById('card')
+
+//Games
+
+function Farcry(){
+  alert("Are you sure want to download?")
+  window.open("")
+  // window.open("magnet:?xt=urn:btih:4CC94E83ED3508524DC02B98DAC1CB8F8F9D9CC3&dn=FarCry&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce");
+}
+
+function Farcry4(){
+  alert("Are you sure want to download?")
+  window.open("")
+  // window.open("magnet:?xt=urn:btih:7DEC2E6B290F227E2EDF13663DB5AE887444A103&dn=farcry+4&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce")
+}
+
+function Fifa19(){
+  alert("Are you sure want to download?")
+  window.open("")
+  // window.open("magnet:?xt=urn:btih:22E2E20D32E05603161600613949D2BCAAD1E04E&dn=FIFA+19+PC+full+game+%5E%5EnosTEAM%5E%5E&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce")
+}
+
+function CounterStrike(){
+  alert("Are you sure want to download?")
+  window.open("")
+  // window.open("magnet:?xt=urn:btih:57919AFEF88D3D919EA45356C0BD353B081CCEEA&dn=Counter+strike+1.6+full+game+with%28bots%2C2500%2Bmaps%2Cmultiplayer%2CLan&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce")
+}
+
+function GoogleChrome(){
+  alert("Are you sure want to download?")
+  window.open("https://www.google.com/chrome/thank-you.html?installdataindex=empty&statcb=1&defaultbrowser=0#")
+}
+
+function office2019(){
+  alert("Are you sure want to download?")
+  window.open("")
+}
+
+function office2007(){
+  alert("Are you sure want to download?")
+  window.open("")
+}
+
+function photoshop2020(){
+  alert("Are you sure want to download?")
+  window.open("")
+}
